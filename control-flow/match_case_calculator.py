cd5153c4f93f477bcccff5377ea1c5eaf14967fa
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-18 03:47:46 +0300

    Wrote code that prints multiplication table for a given input number

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,13 +4,13 @@ operation = input("Choose the operation (+, -, *, /):")
 
 match operation:
     case "+":
-        print("The result is ", num1+num2)
+        print("The result is ", num1 + num2)
     case "-":
         print("The result is ", num1 - num2)
     case "*":
-        print("The result is ", num1*num2)
+        print("The result is ", num1 * num2)
     case "/":
-        if num2 ==0:
+        if num2 == 0:
             print("Cannot divide by zero")
         else:
-            print("The result is ", num1/num2)
+            print("The result is ", num1 / num2)
