314daeacabcbe274d9c01be56b3db0b96cb46cd7
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-19 11:17:32 +0300

    Added functions to convert the temperatures

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,32 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR =  (9/5)
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+
+def convert_to_fahrenheit(celsius):
+     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+temperature = input("Enter the temperature to convert: ")
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+try:
+    temperature = float(temperature)
+
+    if unit.upper() == 'C':
+        result = convert_to_fahrenheit(temperature)
+        print(f"{temperature}°C is {result}°F")
+
+    elif unit.upper() == 'F':
+        result = convert_to_celsius(temperature)
+        print(f"{temperature}°F is {result}°C")
+
+    else:
+        print("Invalid unit. Please enter C or F.")
+
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
+
+         
+
