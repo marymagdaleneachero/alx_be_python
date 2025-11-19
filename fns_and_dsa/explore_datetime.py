26ed5eb9eb2ace40e68130b497892ca929337d8f
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-19 10:39:12 +0300

    Created a script to explore the datetime module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,7 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(print("Current Date and Time:", formatted_date))
+display_current_datetime()
