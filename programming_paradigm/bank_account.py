351d426517ac150425909a2959e8af4173abd607
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-21 06:44:59 +0300

    Implemented methods to handle encapsulation and behaviours

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,4 +2,14 @@ class BankAccount:
     def __init__(self, initial_balance = 0):
         self.account_balance = initial_balance
 
-    
+    def deposit(self,amount):
+        self.account_balance += amount
+
+    def withdraw(self,amount):
+        if self.account_balance > amount:
+            self.account_balance -=amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Your current balance is: ${self.account_balance}")
