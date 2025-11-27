d3b3acba418d4aad2634e8b728c82e9a6901aa79
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-27 14:06:10 +0300

    Added string method to the book class

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,17 +3,20 @@ class Book:
         self.title = title
         self.author = author
 
-class Ebook(Book):
+class EBook(Book):
     def __init__(self, title:str, author:str, file_size:int):
         super().__init__(title,author)
         self.file_size = file_size
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 
 class PrintBook(Book):
     def __init__(self, title:str, author:str, page_count:int):
         super().__init__(title,author)
         self.page_count = page_count
 
-class Libary:
+class Library:
     def __init__(self):
         self.books =[]
     
