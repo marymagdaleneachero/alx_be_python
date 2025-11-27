bc38344f7f3485a6cd4b240b72fb30437cf1d8e7
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-27 13:48:54 +0300

    Implemented a base class and child classes to inherit from it

---
+++ oop/library_system.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, title:str, author:str):
+        self.title = title
+        self.author = author
+
+class Ebook(Book):
+    def __init__(self, title:str, author:str, file_size:int):
+        super().__init__(title,author)
+        self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, title:str, author:str, page_count:int):
+        super().__init__(title,author)
+        self.page_count = page_count
+
+
