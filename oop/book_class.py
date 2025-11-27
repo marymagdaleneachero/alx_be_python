0d73904125b27928a8e1e762cce2af3352d0f690
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-11-27 12:44:48 +0300

    Implemented a string method to represent the object as a string

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,3 +6,6 @@ class Book:
 
     def __del__(self):
         print(f"{self.title} is being deleted")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
